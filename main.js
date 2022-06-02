a204b1d83313b75684b8288b2f7cb37444ce5373
Ezekiel Jeremiah <69088988+EzekielJeremiah@users.noreply.github.com>
2022-06-02 15:51:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const serverUrl = "https://zjd7nc7ttczq.usemoralis.com:2053/server";
+const appId = "s5uqq94zGuGYYCEjWatcy0Eqx2JKtdDSfx9lSY8c";
+Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" });
+      console.log(user);
+      console.log(user.get("ethAddress"));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
